b1e7960f73bf9ccea5e3f0a79a74f479e4a99ada
javierrivarola1 <156439571+javierrivarola1@users.noreply.github.com>
2024-01-12 11:21:15 -0300

    Add files via upload
    Informacion basica de la historia de JavaScript

---
+++ main.js
@@ -0,0 +1,17 @@
+// JAVASCRIPT es un lenguaje de programación que forma parte de las tecnologías básicas de la World Wide Web, junto con HTML y CSS. Nos permite añadir interactividad a las páginas,
+//  también se utiliza en otros entornos que no son navegadores, como Node.js para escribir código del lado del servidor en JavaScript, Electron para escribir aplicaciones de escritorio, React Native para aplicaciones móviles, etc.
+
+// ---------------------------------------------------
+
+// JavaScript fue creado inicialmente por Brendan Eich, en 1995. fue llamado Mocha por el creador, que más tarde fue renombrado LiveScript. 
+// En 1996, aproximadamente un año después del lanzamiento, NetScape decidió renombrarlo a JavaScript
+
+// --------------------------------------------------
+
+// Versiones de JAVASCRIPT : JavaScript, inventado por Brendan Eich, alcanzó el estatus de estándar ECMA en 1997 y adoptó el nombre oficial de ECMAScript. 
+// Este lenguaje ha evolucionado a través de varias versiones, a saber, ES1, ES2, ES3, ES5 y la transformadora ES6. 
+
+// ---------------------------------------------------
+
+// Para almacenar y representar esta información en el código base de JavaScript, utilizamos variables. Una variable es un contenedor de un valor.
+
