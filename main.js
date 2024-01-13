9dbf8ed51f4efda78ec502b66137f0577a80718e
javierrivarola1 <156439571+javierrivarola1@users.noreply.github.com>
2024-01-13 14:35:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,3 +15,24 @@
 
 // Para almacenar y representar esta información en el código base de JavaScript, utilizamos variables. Una variable es un contenedor de un valor.
 
+// Para usar variables en JavaScript, primero debemos crearlas, es decir, declarar una variable. Para declarar variables, utilizamos una de las palabras clave var, let o const.
+
+
+
+// Var ya no se usa mucho, pero seguramente en algun archivo viejo van a encontrar estas variables que son generalmente globales.
+
+
+// Las constantes están limitadas por bloques, al igual que las variables declaradas con la palabra clave let.
+//  El valor de una constante no puede cambiarse mediante reasignación (es decir, utilizando el operador de asignación),
+//  y no puede volver a declararse (es decir, mediante una declaración de variable). 
+// Sin embargo, si una constante es un objeto o una matriz, sus propiedades o elementos pueden actualizarse o eliminarse.
+
+// let declara una variable local de bloque y, opcionalmente, la inicializa con un valor.
+
+// EJEMPLO:
+
+// var holamundo = holaMundo;
+// let holamundo = holaMundo;
+// const holamundo = holaMundo;
+
+// --------------------------------------------
