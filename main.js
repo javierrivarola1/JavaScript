858d30b0d8f9d33ecdeca06bc0b9f87b407a453d
javierrivarola1 <156439571+javierrivarola1@users.noreply.github.com>
2024-01-22 16:01:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -53,6 +53,23 @@
 
 // --------------------------------------------
 
+// Operadores de comparación
+
+// Los operadores de comparación se utilizan en declaraciones lógicas para determinar la igualdad o diferencia entre variables o valores.
+
+// == es igual a parecidos.
+
+// ejemplo: x == 5
+// x == "5"
+// Los 2 resultados son verdaderos ya que el numero 5 aparece mas alla que sea un numero o un string, JavaScrpit toma como resultado correcto
+// el numero 5
+
+
+
+
+
+// 
+
 // El operador typeof devuelve el tipo de una variable, objeto, función o expresión:
 
 // EJEMPLOS:
@@ -131,3 +148,42 @@
 // const nombresGeniales = nombres.map(nombre => nombre + " es genial");
 // console.log(nombresGeniales);
 // Javier es genial.
+
+// JSON
+
+// La notación de objetos JavaScript (JSON) es un formato estándar basado en texto para representar datos estructurados basados ​​en la sintaxis
+// de objetos JavaScript. Se usa comúnmente para transmitir datos en aplicaciones web (por ejemplo,
+//  enviar algunos datos desde el servidor al cliente, para que puedan mostrarse en una página web, o viceversa).
+
+// EJEMPLO
+
+// archivo squadname.JSON
+
+// {
+// "squadName": "Super hero squad",
+// "homeTown": "Metro City",
+// "formed": 2016,
+// "secretBase": "Super tower",
+// "active": true,
+// "members": [
+//     {
+//       "name": "Molecule Man",
+//       "age": 29,
+//       "secretIdentity": "Dan Jukes",
+//       "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+//     },
+//     {
+//       "name": "Madame Uppercut",
+//       "age": 39,
+//       "secretIdentity": "Jane Wilson",
+//       "powers": [
+//         "Million tonne punch",
+//         "Damage resistance",
+//         "Superhuman reflexes"
+//       ]
+//     },
+// ],
+// }
+
+// 
+
